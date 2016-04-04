2559c5dfe857434f847c82473e7a29c90ff9d945
Marcus Vinicius Gonçalves <marquinhusgoncalves@hotmail.com>
2016-04-04 09:42:07 -0300

    Erro no e-mail

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <?php
 $subjectPrefix = 'Site Stephannie Print';
-$emailTo = '<contato@stephannie.com.br>';
+$emailTo = '<contato@stephannieprint.com.br>';
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
     $name    = stripslashes(trim($_POST['form-name']));
     $email   = stripslashes(trim($_POST['form-email']));
